e059cfae9602904660430c0c82e954f540e2b55f
David Fernández <david@mundosaparte.com>
2026-03-16 21:14:06 +0100

    Update cask for v0.1.0

--- Casks/minimaleditor.rb
+++ Casks/minimaleditor.rb
@@ -1,6 +1,6 @@
 cask "minimaleditor" do
-  version "0.1.6"
-  sha256 "ec3d4197900bb743739f0160a41b2c05206bf802c7505bce637ac2980c388f42"
+  version "0.1.0"
+  sha256 "c664d663599c37c851dbeba1e81d4d066de693ba031f044bf32838a983a9f27d"
 
   url "https://github.com/driade/minimal-editor/releases/download/v#{version}/MinimalEditor-macOS.zip"
   name "MinimalEditor"
